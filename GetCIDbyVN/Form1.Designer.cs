a1365a34beb1dfd8dfc7687e5ab5a41e6462f00c
wpyok500 <wpyok168@gmail.com>
2023-04-03 23:09:45 +0800

    添加项目文件。

---
+++ GetCIDbyVN/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace GetCIDbyVN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(879, 35);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 148);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(879, 35);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "安装ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "确认ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(408, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 249);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1077, 320);
+            this.MinimumSize = new System.Drawing.Size(1077, 320);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "获取确认ID by 越南";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GetCIDbyVN/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Web.Hosting;
+using System.IO.Compression;
+using System.Runtime.InteropServices.ComTypes;
+using System.IO;
+using System.Security.Policy;
+
+namespace GetCIDbyVN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Clear();
+            string iid = Clipboard.GetText().Replace(" ", "").Replace("-", "");
+            if (Regex.IsMatch(iid, "[\\d]{63}"))
+            {
+                this.textBox1.Text = Regex.Match(iid, "[\\d]{63}").Value;
+                GetCID(this.textBox1.Text);
+            }
+            else if (Regex.IsMatch(iid, "[\\d]{54}"))
+            {
+                this.textBox1.Text = Regex.Match(iid, "[\\d]{54}").Value;
+                GetCID(this.textBox1.Text);
+            }
+            else
+            {
+                this.textBox1.Text = "安装ID有误";
+            }
+        }
+
+        private async void GetCID(string iid)
+        {
+           await  Task.Factory.StartNew(() => {
+               this.label3.Invoke(new Action(() =>
+               {
+                   this.label3.Text = "正在获取，请耐心等待下。。。。";
+                   this.label3.Visible = true;
+               }));
+                
+                string crtiid = string.Empty;
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Mode = CipherMode.CBC;
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.IV = Encoding.UTF8.GetBytes("7061759328313233");
+                    aes.Key = Encoding.UTF8.GetBytes("ditObg4239Ajdk@d");
+                    ICryptoTransform ct = aes.CreateEncryptor(aes.Key, aes.IV);
+                    byte[] encrypted;
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, ct, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                swEncrypt.Write(iid);
+                            }
+                            encrypted = msEncrypt.ToArray();
+                        }
+                    }
+                    crtiid = Convert.ToBase64String(encrypted);
+
+                }
+                string url1 = $"https://0xc004c008.com";
+                string url2 = $"https://0xc004c008.com/ajax/get_cid?iids={System.Web.HttpUtility.UrlEncode(crtiid)}";
+                string cookie = GetCookie(url1);
+                HttpWebRequest hwr = (HttpWebRequest)HttpWebRequest.Create(url2);
+                hwr.Method = "GET";
+                hwr.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n";
+                hwr.KeepAlive = true;
+                hwr.AllowAutoRedirect = true;
+                hwr.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
+                hwr.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+                hwr.Headers.Add("accept-encoding", "gzip, deflate");//, br
+                hwr.Headers.Add("accept-language", "zh-CN,zh;q=0.9,en;q=0.8,ru;q=0.7,de;q=0.6");
+                //hwr.Headers.Add("Cookie", "userlogin=usersettings=6Y6J10PXuqzKxk6kYtsmQNb8QZLN41TB3BfFXKBOhw/nWUqqE5UVkpzMe8jRCDP92w7zAl/PnPTRHax/O50kDdZNzG1OoCSDmKDYc1YQFquyiFly3LLp/9lg+thKbYeiBpNrHGKyOkBsicVNQUSKtEBOjqW9eAIkVyuWUrqLIGeCOAPW4EreXXA/FJeexYStlkga8shEG7YhZo2q7fHrP+JU3eEEwkongW6MUDbTSWBAgj7v7lDjUSwtGdKVrT05u8ZjBOKPrXO2XHMC/jsspOKDQ0fit6vMA0ut2Lh1pNS/lZhcD2L1JLoQyYi9hTiGmAAFygjBW+Ner8555gsVTg==\r\n\r\n");
+                hwr.Headers.Add("Cookie", cookie);
+                HttpWebResponse hws;
+                string outhtml = string.Empty;
+                string plaintext = string.Empty;
+                try
+                {
+                    using (hws = (HttpWebResponse)hwr.GetResponse())
+                    {
+                        if (hws.ContentEncoding.ToLower().Contains("gzip"))
+                        {
+                            outhtml = new StreamReader(new GZipStream(hws.GetResponseStream(), CompressionMode.Decompress)).ReadToEnd();
+                        }
+                        else if (hws.ContentEncoding.ToLower().Contains("deflate"))
+                        {
+                            outhtml = new StreamReader(new DeflateStream(hws.GetResponseStream(), CompressionMode.Decompress)).ReadToEnd();
+                        }
+                        //else if (hws.ContentEncoding.ToLower().Contains("br"))
+                        //{
+                        //    //outhtml = new StreamReader(new brstream(hws.GetResponseStream(), CompressionMode.Decompress)).ReadToEnd();
+                        //}
+                        else
+                        {
+                            outhtml = new StreamReader(hws.GetResponseStream()).ReadToEnd();
+                        }
+                        if (!string.IsNullOrEmpty(outhtml))
+                        {
+                            using (Aes aes = Aes.Create())
+                            {
+                                aes.Mode = CipherMode.CBC;
+                                aes.Padding = PaddingMode.PKCS7;
+                                aes.IV = Encoding.UTF8.GetBytes("7061759328313233");
+                                aes.Key = Encoding.UTF8.GetBytes("ditObg4239Ajdk@d");
+                                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(outhtml)))
+                                {
+                                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                                    {
+                                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                                        {
+                                            plaintext = srDecrypt.ReadToEnd();
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                    System.Web.Script.Serialization.JavaScriptSerializer js = new System.Web.Script.Serialization.JavaScriptSerializer();
+                    Dictionary<string, string> dy = js.Deserialize<Dictionary<string, string>>(plaintext);
+                    if (dy["result"].Equals("Successfully"))
+                    {
+                       this.textBox2.Invoke(new Action(() =>
+                       {
+                           this.textBox2.Text = dy["confirmationid"];
+                       }));
+                       
+                    }
+                    else
+                    {
+                       this.textBox2.Invoke(new Action(() =>
+                       {
+                           this.textBox2.Text = dy["short_result"];
+                       }));
+                       
+                    }
+
+                   Invoke((Action)(() => { Clipboard.SetText("安装ID：" + this.textBox1.Text + "\r\n" + "确认ID：" + this.textBox2.Text); }));
+                   
+                   this.label3.Invoke(new Action(() =>
+                   {
+                       this.label3.Text = "已获取并复制到了剪切板";
+                   }));
+                   
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+
+            });
+            
+        }
+        private string GetCookie(string url)
+        {
+            HttpWebRequest hwr = (HttpWebRequest)HttpWebRequest.Create(url);
+            hwr.Method = "GET";
+            hwr.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n";
+            hwr.KeepAlive = true;
+            hwr.AllowAutoRedirect = true;
+            hwr.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
+            hwr.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            hwr.Headers.Add("accept-encoding", "gzip, deflate, br");
+            hwr.Headers.Add("accept-language", "zh-CN,zh;q=0.9,en;q=0.8,ru;q=0.7,de;q=0.6");
+            //hwr.Headers.Add("Cookie", "userlogin=usersettings=6Y6J10PXuqzKxk6kYtsmQNb8QZLN41TB3BfFXKBOhw/nWUqqE5UVkpzMe8jRCDP92w7zAl/PnPTRHax/O50kDdZNzG1OoCSDmKDYc1YQFquyiFly3LLp/9lg+thKbYeiBpNrHGKyOkBsicVNQUSKtEBOjqW9eAIkVyuWUrqLIGeCOAPW4EreXXA/FJeexYStlkga8shEG7YhZo2q7fHrP+JU3eEEwkongW6MUDbTSWBAgj7v7lDjUSwtGdKVrT05u8ZjBOKPrXO2XHMC/jsspOKDQ0fit6vMA0ut2Lh1pNS/lZhcD2L1JLoQyYi9hTiGmAAFygjBW+Ner8555gsVTg==\r\n\r\n");
+            HttpWebResponse hws;
+            string outhtml = string.Empty;
+            try
+            {
+                using (hws = (HttpWebResponse)hwr.GetResponse())
+                {
+                    //return hws.Headers.Get("Set-Cookie");
+                    return hws.Headers[HttpResponseHeader.SetCookie];
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(this.textBox2.Text);
+        }
+    }
+}
